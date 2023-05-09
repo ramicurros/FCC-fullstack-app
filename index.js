bec03370a6b6c37c6f5e836a81b3be7b996d402a
ramicurros <ramicurros@gmail.com>
2023-05-09 13:00:07 -0300

    fcc test commit

--- index.js
+++ index.js
@@ -28,23 +28,13 @@ const newUserSchema = new Schema({
   }
 })
 
-const excerciseSchema = new Schema({
-  description: {
-    type: String,
-    required: true
-  },
-  duration: {
-    type: Number,
-    required: true
-  },
-  date: {
-    type: String,
-    default: new Date().toDateString()
-  }
-});
 
 const logSchema = new Schema({
   username: {
+    type: String,
+    required: true
+  },
+  _id: {
     type: String,
     required: true
   },
@@ -59,11 +49,13 @@ const logSchema = new Schema({
 })
 
 let userList = [];
+var userLogList = [];
+
 let User = mongoose.model('User', newUserSchema);
-let Excercise = mongoose.model('Excercise', excerciseSchema);
 let UserLog = mongoose.model('UserLog', logSchema);
 
-const createNSaveUser = async (user, id) => {
+
+const createNSaveUser = async (user, id, logId) => {
   const data = new User({ username: user, _id: id });
   try {
     let output;
@@ -86,8 +78,8 @@ const createNSaveExcercise = async (desc, duration, date) => {
   }
 }
 
-const updateLog = async (user, excercise) => {
-  const userLog = await UserLog.findOne({username: user});
+const updateLog = async (userId, excercise) => {
+  const userLog = await UserLog.findById(userId);
   userLog.count = userLog.count + 1;
   userLog.log.push(excercise);
   userLog.markModified('userLog.count');
@@ -96,19 +88,21 @@ const updateLog = async (user, excercise) => {
   return userLog;
 }
 
-const doLog = async (user, excercise) => {
+const doLog = async (id, excercise) => {
   let log;
-  const item = await UserLog.findOne({username: user});
-  console.log(`found log: ${item}`)
+  const user = await getUser(id);
+  console.log(`user: ${user}`);
+  const item = await UserLog.findById(id);
+  console.log(`log founded: ${item}`)
   if(!item){
-    let data = new UserLog({username: user, count: 1,log: [excercise]});
+    let data = new UserLog({username: user.username, _id: id, count: 1, log: [excercise]});
     try {
       log = await data.save();
     } catch (error) {
       console.log(`error: ${error.message}`)
     }
   } else {
-    log = await updateLog(user, excercise);
+    log = await updateLog(id, excercise);
   }
   console.log(`log: ${log}`)
   return log
@@ -151,7 +145,8 @@ app.get('/', (req, res) => {
 });
 
 app.route('/api/users').post(async (req, res) => {
-  let id = new ObjectId();
+  let id = new ObjectId().toString();
+  req.query.logId = new ObjectId().toString();
   let user = await createNSaveUser(req.body.username, id);
   res.json({username: user.username, _id: id});
 }).get((req, res) => {
@@ -169,21 +164,18 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
   let user = await getUser(req.body[':_id']);
   console.log(`user found ${user}`)
   if (!user) res.json({ error: 'Invalid Id' });
-  excercise = await createNSaveExcercise(req.body.description, req.body.duration, date);
-  let updatedUser = await updateUser(req.body[':_id'], {...excercise._doc})
-  await doLog(user.username, {...excercise._doc});
-  console.log(`excercise json: ${excercise}}`);
+  excercise = {description: req.body.description, duration: req.body.duration, date: date}
+  let updatedUser = await updateUser(req.body[':_id'], excercise)
+  await doLog(req.body[':_id'], excercise);
+  console.log(`excercise json: ${excercise}`);
   res.json(updatedUser);
 });
 
 app.get('/api/users/:_id/logs', async (req, res) => {
   let output;
-  let user = await getUser(req.params._id);
   try {
-    const log = await UserLog.findOne({username: user.username});
+    output = await UserLog.findById(req.params._id);
     console.log(`id ${req.params._id}`);
-    console.log(`log ${log}`);
-    output = {...log._doc, _id: req.params.id};
     console.log(`output ${output}`);
   } catch (error) {
     output = {error: error.message};
